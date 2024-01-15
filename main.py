d1dab9ea4934a1f29a83d6a84132b7d0c43d5ae4
Kirillsimakov <156677467+Kirillsimakov@users.noreply.github.com>
2024-01-15 16:14:45 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+# import turtle
+#
+# robert = turtle.Turtle()
+# robert.speed(100)
+# def pyatiugolnik():
+#     for i in range(6):
+#         robert.forward(100)
+#         robert.left(72)
+#
+# for i in range(205):
+#     pyatiugolnik()
+#     robert.color('red')
+#     robert.left(2005)
+#     robert.forward(100)
+#     robert.color('green')
+#
+# turtle.done()
+
+# import turtle
+#
+# robert = turtle.Turtle()
+# robert.speed(10000000000000000000000000000000000000)
+#
+# for i in range(100000000000000):
+#     robert.circle(i)
+#     robert.color('black')
+#     robert.forward(135)
+#     robert.color('red')
+#     robert.left(567)
+#
+#
+#
+# turtle.done()
